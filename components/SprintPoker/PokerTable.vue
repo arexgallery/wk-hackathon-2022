72d945433a49de9bc9c58734094cf6ddb45beb71
WBK\tibeau.schodts <tibeau.schodts@wolterskluwer.com>
2022-06-08 11:19:30 +0200

    Fixes

--- components/SprintPoker/PokerTable.vue
+++ components/SprintPoker/PokerTable.vue
@@ -1,30 +1,30 @@
+<script>
+import TableSeat from './TableSeat'
+export default {
+  components: { TableSeat },
+}
+</script>
+
 <template>
   <div class="flex">
     <div class="flex flex-col items-center justify-center">
-      <TableSeat></TableSeat>
+      <TableSeat />
     </div>
     <div class="flex flex-col">
       <div class="flex justify-between mx-12">
-        <TableSeat></TableSeat>
-        <TableSeat></TableSeat>
-        <TableSeat></TableSeat>
+        <TableSeat />
+        <TableSeat />
+        <TableSeat />
       </div>
-      <div class="m-4 min-w-[380px] h-[140px] bg-gray-300 rounded-full"></div>
+      <div class="m-4 min-w-[380px] h-[140px] bg-gray-300 rounded-full" />
       <div class="flex justify-between mx-12">
-        <TableSeat></TableSeat>
-        <TableSeat></TableSeat>
-        <TableSeat></TableSeat>
+        <TableSeat />
+        <TableSeat />
+        <TableSeat />
       </div>
     </div>
     <div class="flex flex-col items-center justify-center">
-      <TableSeat></TableSeat>
+      <TableSeat />
     </div>
   </div>
 </template>
-
-<script>
-import TableSeat from './TableSeat';
-export default {
-  components: {TableSeat}
-}
-</script>
